39034d4cd211b15ac8769ec1c77d6d65572d37e0
Petru Cioata <petru.cioata@assist.ro>
2018-10-26 10:20:00 +0300

    Added methods in context of an object

---
+++ October 26,2018/methods_creation/methods_in_context_of_object.rb
@@ -0,0 +1,29 @@
+# create an instance of Object
+first_object = Object.new
+
+# define setter method for this instance
+def first_object.set_variable=(value)
+  @instance_variable = value
+end
+
+# define getter method for this instance
+def first_object.get_variable
+  p @instance_variable
+end
+
+# call methods on first_object
+first_object.get_variable
+first_object.set_variable = "Hello First Object"
+first_object.get_variable
+
+# create a second_object and try methods defined
+second_object = Object.new
+
+p second_object.instance_variable # undefined method `get_variable' for #<Object:0xyz> (NoMethodError)
+second_object.instance_variable = "Hello Second Object"
+p second_object.instance_variable
+
+class Object
+  attr_writer :instance_variable
+  attr_reader :instance_variable
+end
